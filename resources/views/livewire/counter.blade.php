048a644b4310a3fb46867f0849e0454c6ee6069b
isaiasvas <78052574+isaiasvas@users.noreply.github.com>
2023-01-06 03:20:47 -0300

    Update counter.blade.php

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,17 @@
 <div>
-    <h1>Hello World!</h1>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+        hello
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </div>
