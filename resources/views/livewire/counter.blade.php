fd495dda07ef8e223ee6a22ad00a99a2de21d28f
isaiasvas <78052574+isaiasvas@users.noreply.github.com>
2023-01-06 03:22:20 -0300

    Update counter.blade.php

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -2,11 +2,21 @@
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
         <div class="container-fluid py-4">
             <div class="row">
-                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                <div class="col-lg-7 mb-lg-0 mb-4">
                     <div class="card">
                         <div class="card-body p-3">
                             <div class="row">
-        hello
+                                <div class="col-lg-6">
+                                    <div class="d-flex flex-column h-100">
+                                        <p class="mb-1 pt-2 text-bold">Built by developers</p>
+                                        <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
+                                        <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
+                                        <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
+                                            Read More
+                                            <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
