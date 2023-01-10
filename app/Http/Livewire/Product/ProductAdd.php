a8909347ae6d8c2295dd2e93e751d2b667bd3d61
isaiasvas <isaias@visioit.com.br>
2023-01-09 21:06:26 -0300

    product

---
+++ app/Http/Livewire/Product/ProductAdd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use Livewire\Component;
+
+class ProductAdd extends Component
+{
+    public function render()
+    {
+        return view('livewire.product.product-add');
+    }
+}
