14196bc3ea9af87122f75290ed4cd39304690594
isaiasvas <isaias@visioit.com.br>
2023-02-14 21:20:47 -0300

    oi

--- app/Http/Livewire/Product/ProductAdd.php
+++ app/Http/Livewire/Product/ProductAdd.php
@@ -42,9 +42,10 @@ class ProductAdd extends Component
             'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
             'images' => $images
         ];
-        
+
         dump($data);
-        $product = Product::create($data);
+        
+        //$product = Product::create($data);
     }
     public function render()
     {
