a40c45f903468ed5232e4169b40ff8463bb99586
isaiasvas <isaias@visioit.com.br>
2023-02-15 00:57:53 -0300

    oi

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,6 +40,16 @@ return [
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
+            'permissions' => [
+                'file' => [
+                    'public' => 0777,
+                    'private' => 0777,
+                ],
+                'dir' => [
+                    'public' => 0777,
+                    'private' => 0777,
+                ],
+            ],
         ],
 
         'real_public' => [
