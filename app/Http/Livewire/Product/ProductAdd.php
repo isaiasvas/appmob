1fbd095e8f4d1019ad0e6d7806c3113c02bc9312
isaiasvas <isaias@visioit.com.br>
2023-02-14 22:59:58 -0300

    oi

--- app/Http/Livewire/Product/ProductAdd.php
+++ app/Http/Livewire/Product/ProductAdd.php
@@ -39,19 +39,12 @@ class ProductAdd extends Component
             'regular_price' => $price,
             'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
             'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-            'images2' => $images,
-            'images' => [
-                [
-                    'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-                ],
-                [
-                    'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-                ]
-            ]
+            'images' => $images,
+            
         ];
 
         dump($data);
-        //$product = Product::create($data);
+        $product = Product::create($data);
     }
     public function render()
     {
