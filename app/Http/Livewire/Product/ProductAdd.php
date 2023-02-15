7153a6d768d6167977efad265d992fd18d5464c9
isaiasvas <isaias@visioit.com.br>
2023-02-14 22:43:22 -0300

    oi

--- app/Http/Livewire/Product/ProductAdd.php
+++ app/Http/Livewire/Product/ProductAdd.php
@@ -20,7 +20,7 @@ class ProductAdd extends Component
         $name = $this->product_categories . ' ' . $this->product_sub_categories;
         $price = $this->product_price;
         $getImages = $this->product_images;
-        $images = [];
+        $images = ["images"];
         foreach ($getImages as $photo) {
             $image = $photo;
             $name_gen = rand(10,1000000000).'.'.$image->getClientOriginalExtension();
@@ -38,9 +38,7 @@ class ProductAdd extends Component
             'regular_price' => $price,
             'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
             'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
-            'images' => [
-                    $images
-                ]
+            $images,
         ];
 
         dump($data);
